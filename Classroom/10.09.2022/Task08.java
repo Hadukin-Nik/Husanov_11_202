5826506bc8932ee098f1fc036c30db12e986cc89
Nikolay <k@email.com>
2022-09-10 13:22:43 +0300

    Complited Tasks 1-14

--- Classroom/10.09.2022/Task08.java
+++ Classroom/10.09.2022/Task08.java
@@ -1,32 +1,21 @@
 public class Task08{
 	public static void main (String[] args) {
-		//float x = Float.parseFloat(args[0]);
-		//float y = Float.parseFloat(args[1]);
+		float x = Float.parseFloat(args[0]);
+		float y = Float.parseFloat(args[1]);
 
 		
-		for(double y = 10; y >= -10; y -= 0.21) {
-			for(double x = -10; x <= 10; x += 0.12) {
-				if(x <= 0 && x*x + (y - 2)*(y - 2) > 4 && x*x + y*y < 16 || x*x + (y + 2)*(y + 2) < 4) {
-		   			if (!(x*x + (y + 2)*(y + 2) < 1)) {
-		   				System.out.print(1);
-		   			} else {
-		   				System.out.print(0);
-		   			}
-				} else if (x*x + y*y < 16) {
-    				if ((x*x + (y - 2)*(y - 2) < 1)) {
-		   				System.out.print(1);
-		   			} else {
-		   				System.out.print(0);
-		   			}
-				} else {
-					System.out.print(" ");
-				}
-	    		
-
+		if(x <= 0 && x*x + (y - 2)*(y - 2) > 4 && x*x + y*y < 16 || x*x + (y + 2)*(y + 2) < 4) {
+		   	if (!(x*x + (y + 2)*(y + 2) < 1)) {
+		   		System.out.print(1);
+			} else {
+				System.out.print(0);
 			}
-
-			System.out.println();
-
+		} else if (x*x + y*y < 16) {
+    		if ((x*x + (y - 2)*(y - 2) < 1)) {
+		   		System.out.print(1);
+		   	} else {
+		   		System.out.print(0);
+		   	}
 		}
 	}
 }
