c660b9418ec6de6f162c4b20a9495c14d354e541
Nikolay <k@email.com>
2022-09-17 12:39:29 +0300

    Task 15 - 20 completed for check

--- Classroom/17.09.2022/Draft.java
+++ Classroom/17.09.2022/Draft.java
@@ -1,5 +1,20 @@
 public class Draft {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+		double x = Double.parseDouble(args[0]);
 
+		Long lastInUseK = 1L;
+		double lastInUseX = 1.0;
+		double eps = 0.000001;
+
+		double ans = 0.0;
+
+		for (int k = 1; lastInUseX / lastInUseK >= eps; k++) {
+			lastInUseK *= k;
+			lastInUseX *= x;
+
+			ans += lastInUseX / lastInUseK;
+		}
+
+		System.out.println(ans - eps);
 	}
 }
