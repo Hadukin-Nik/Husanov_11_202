735b78f6b3ff11a5fd21a6473a95bf96f3e91a55
Nikola <husanov.nik@gmail.com>
2023-04-01 07:16:13 +0300

    Realized all tasks of homework and also added to HomeWork folder, also copied folder with tasks with comparators to HomeWork

---
+++ 2S/HomeWork/02_March/2023.03.25/DataBase.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DataBase {
+    String subscribtionsAddress;
+    String usersAddress;
+
+    List<User> users;
+
+    public DataBase(String subscribtionsAddress, String usersAddress) {
+        this.subscribtionsAddress = subscribtionsAddress;
+        this.usersAddress = usersAddress;
+
+        users = null;
+    }
+
+    public List<User> getUsers() {
+        if(users != null) return users;
+        List<User> users = new ArrayList<>();
+        Scanner sc = new Scanner(usersAddress);
+        while(sc.hasNext("[A-Za-z]* [A-za-z]* [1-9]* [1-9]*")) {
+            String[] buf = sc.next().split(" ");
+            users.add(new User(buf[0], Integer.parseInt(buf[2]), Integer.parseInt(buf[3])));
+        }
+
+        return users;
+    }
+
+    public User getUserById(int id) {
+        return getUsers().stream().filter(user -> {return user.getId() == id;}).findFirst().get();
+    }
+
+    public List<Subscribe> getSubscibtions() {
+        List<Subscribe> subscriptions = new ArrayList<>();
+
+        Scanner sc = new Scanner(subscribtionsAddress);
+
+        List<User> users = getUsers();
+
+        while(sc.hasNext("[1-9]* [1-9]*")) {
+            String[] buf = sc.next().split(" ");
+            subscriptions.add(new Subscribe(getUserById(Integer.parseInt(buf[0])), getUserById(Integer.parseInt(buf[1]))));
+        }
+
+        return subscriptions;
+    }
+}
