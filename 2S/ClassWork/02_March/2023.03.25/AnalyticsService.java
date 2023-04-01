c32726ced3ef3339fe114d683603a15c007471b0
Nikola <husanov.nik@gmail.com>
2023-04-01 07:15:47 +0300

    Realized all tasks of homework and also added to HomeWork folder, also copied folder with tasks with comparators to HomeWork

---
+++ 2S/ClassWork/02_March/2023.03.25/AnalyticsService.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class AnalyticsService {
+    DataBase dataBase;
+
+    public AnalyticsService(DataBase dataBase) {
+        this.dataBase = dataBase;
+    }
+
+    public List<String> getLastNamesBeforeYear(int year) {
+        List<User> users = dataBase.getUsers();
+
+        return users.stream().filter(user -> {return user.getYear() <= year;}).map(User::getLastName).toList();
+    }
+
+    public int getNumberByYear(int year) {
+        List<User> users = dataBase.getUsers();
+
+        return users.stream().filter(user -> {return user.getYear() == year;}).toArray().length;
+    }
+
+    public List<String> getSubscriptionsNames(int userId) {
+        List<Subscribe> subr = dataBase.getSubscibtions();
+        return subr.stream().filter(user -> {return user.getWho().getId() == userId;})
+                    .map(Subscribe::getWhom).map(User::getName).toList();
+    }
+
+    public List<String> getSubscribersNames(int userId) {
+        List<Subscribe> subr = dataBase.getSubscibtions();
+        return subr.stream().filter(user -> {return user.getWhom().getId() == userId;})
+                .map(Subscribe::getWho).map(User::getName).toList();
+    }
+
+    public List<String> getFriends(int userId) {
+        List<Subscribe> subr = dataBase.getSubscibtions();
+        return subr.stream().filter(subscribe -> {return
+                subscribe.getWho().getId() == userId
+                &&
+                subr.contains(new Subscribe(subscribe.getWhom(), subscribe.getWho()));})
+                .map(Subscribe::getWhom).map(User::getName).toList();
+    }
+}
---
+++ 2S/ClassWork/02_March/2023.03.25/Subscribe.java
@@ -0,0 +1,17 @@
+public class Subscribe {
+    private User who;
+    private User whom;
+
+    public Subscribe(User who, User whom) {
+        this.who = who;
+        this.whom = whom;
+    }
+
+    public User getWho() {
+        return who;
+    }
+
+    public User getWhom() {
+        return whom;
+    }
+}
