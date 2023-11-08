ad974920a4e7678fb06822f72139fb5a0ce483ec
husan <husanov.nik@gmail.com>
2023-11-08 20:28:52 +0300

    Added a configuration file

---
+++ 3S/Projects/MathHelper/src/main/java/org/example/configuration/BotConfig.java
@@ -0,0 +1,10 @@
+package org.example.configuration;
+
+import lombok.Data;
+import lombok.Value;
+
+@Data
+public class BotConfig {
+    String botName = "MarhHelper_bot";
+    String token = "6802520833:AAH_CzagxF5h9gfSstnsrvGCC5D7jJp6uls";
+}
