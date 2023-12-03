aca4bf6cc7809b5b73af08b28a0a66cb99046a95
husan <husanov.nik@gmail.com>
2023-12-03 23:20:23 +0300

    Added basic entity

---
+++ 3S/Projects/AgarIO/src/main/java/com/example/agario/game/Entity.java
@@ -0,0 +1,54 @@
+package com.example.agario.game;
+
+import com.example.agario.helpers.Vector2D;
+
+public class Entity {
+    private Vector2D location;
+    private double radius;
+
+    public Entity() {
+        location = new Vector2D();
+        radius = 1;
+    }
+
+    public Entity(Vector2D location) {
+        this.location = location;
+        radius = 1;
+    }
+
+    public Entity(Vector2D location, double radius) {
+        this(location);
+        this.radius = radius;
+    }
+
+
+
+    public Vector2D getLocation() {
+        return location;
+    }
+
+    public void setLocation(Vector2D location) {
+        this.location = location;
+    }
+
+    public void move(Vector2D addable) {
+        this.location.add(addable);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void addRadius(double addable) {
+        this.radius += radius;
+    }
+
+    public void subtractRadius(double subtrahend) {
+        this.radius -= subtrahend;
+    }
+
+}
