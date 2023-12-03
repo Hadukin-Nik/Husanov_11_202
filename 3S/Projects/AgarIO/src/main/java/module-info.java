698713d3f06277392683ae71e3d9a6de81a58719
husan <husanov.nik@gmail.com>
2023-12-03 23:22:01 +0300

    Changes in GameWindow.fxml and module-info relatively

--- 3S/Projects/AgarIO/src/main/java/module-info.java
+++ 3S/Projects/AgarIO/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.example.agario {
+module com.example.agario.frontend {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.agario to javafx.fxml;
-    exports com.example.agario;
+    opens com.example.agario.frontend to javafx.fxml;
+    exports com.example.agario.frontend;
 }
